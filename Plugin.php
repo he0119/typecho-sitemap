78acd2129a0d56b2ff0a8ee4acf5db6c18809209
victoryss <info@a696.com>
2017-03-28 22:01:31 +0800

    first commit

---
+++ Plugin.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * 网站 Sitemap 生成器
+ * 
+ * @package Sitemap
+ * @author Hanny
+ * @version 1.0.3
+ * @dependence 9.9.2-*
+ * @link http://www.imhan.com
+ *
+ * 历史版本
+ * version 1.0.3 at 2017-03-28
+ * 【禾令奇】www.helingqi.com 修改增加标签链接，修改页面权重分级。
+ *
+ * version 1.0.1 at 2010-01-02
+ * 修改自定义静态链接时错误的Bug
+ *
+ * version 1.0.0 at 2010-01-02
+ * Sitemap for Google
+ * 生成文章和页面的Sitemap
+ *
+ */
+class Sitemap_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+		Helper::addRoute('sitemap', '/sitemap.xml', 'Sitemap_Action', 'action');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+	{
+		Helper::removeRoute('sitemap');
+	}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+}
