863f86bb42ecba462c8b0891a5b0ba82d3dc9754
uy/sun <hmy0119@gmail.com>
2025-11-07 15:15:35 +0800

    feat: 基于 joyqi 的版本修改 (#2)
    * feat: 基于 joyqi 的版本修改
    
    * feat: 美化 XML 显示
    
    * chore: 修改文件名称
    
    * feat: 跳过对加密文章
    
    确保生成的站点地图只包含可访问的内容
    
    * fix: 更新插件版本号为 1.0.0
    
    * fix: 更新插件文档链接至正确的 GitHub 地址
    
    * feat: 跳过隐藏页面
    
    * refactor: 使用箭头函数简化密码检查逻辑
    
    * fix: 不需要检查创建时间

---
+++ Generator.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace TypechoPlugin\Sitemap;
+
+use Widget\Base\Contents;
+use Widget\Contents\Page\Rows;
+use Widget\Contents\Post\Recent;
+use Widget\Metas\Category\Rows as CategoryRows;
+use Widget\Metas\Tag\Cloud;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+/**
+ * Sitemap Generator
+ */
+class Generator extends Contents
+{
+    /**
+     * @return void
+     */
+    public function generate()
+    {
+        $sitemap = '<?xml version="1.0" encoding="' . $this->options->charset . '"?>' . PHP_EOL;
+        $sitemap .= "<?xml-stylesheet type='text/xsl' href='" . $this->options->pluginUrl . "/Sitemap/sitemap.xsl'?>" . PHP_EOL;
+        $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'
+            . ' xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"'
+            . ' xmlns:xhtml="http://www.w3.org/1999/xhtml"'
+            . ' xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"'
+            . ' xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">' . PHP_EOL;
+
+        // add homepage
+        $sitemap .= <<<EOF
+        <url>
+            <loc>{$this->options->siteUrl}</loc>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+EOF;
+
+        // add posts
+        if (in_array('posts', $this->options->plugin('Sitemap')->sitemapBlock)) {
+            $postsCount = $this->size($this->select()
+                ->where('table.contents.status = ?', 'publish')
+                ->where('table.contents.created < ?', $this->options->time)
+                ->where('table.contents.type = ?', 'post'));
+
+            $posts = Recent::alloc(['pageSize' => $postsCount]);
+            $freq = $this->options->plugin('Sitemap')->updateFreq === 'monthly' ? 'monthly' : 'weekly';
+
+            while ($posts->next()) {
+                // 如果加密则跳过
+                if ($posts->password)
+                    continue;
+
+                $sitemap .= <<<EOF
+            <url>
+                <loc>{$posts->permalink}</loc>
+                <changefreq>{$freq}</changefreq>
+                <lastmod>{$posts->date->format('c')}</lastmod>
+                <priority>0.8</priority>
+            </url>
+EOF;
+            }
+        }
+
+        // add pages
+        if (in_array('pages', $this->options->plugin('Sitemap')->sitemapBlock)) {
+            $pages = Rows::alloc();
+            $freq = $this->options->plugin('Sitemap')->updateFreq === 'monthly' ? 'yearly' : 'monthly';
+
+            while ($pages->next()) {
+                // 如果隐藏则跳过
+                if ($pages->status == 'hidden')
+                    continue;
+
+                $sitemap .= <<<EOF
+            <url>
+                <loc>{$pages->permalink}</loc>
+                <changefreq>{$freq}</changefreq>
+                <lastmod>{$pages->date->format('c')}</lastmod>
+                <priority>0.5</priority>
+            </url>
+EOF;
+            }
+        }
+
+        // add categories
+        if (in_array('categories', $this->options->plugin('Sitemap')->sitemapBlock)) {
+            $categories = CategoryRows::alloc();
+            $freq = $this->options->plugin('Sitemap')->updateFreq;
+
+            while ($categories->next()) {
+                // 检查该分类下是否有非加密文章
+                $postsInCategory = $this->db->fetchAll(
+                    $this->db->select('password')
+                        ->from('table.contents')
+                        ->join('table.relationships', 'table.contents.cid = table.relationships.cid')
+                        ->where('table.relationships.mid = ?', $categories->mid)
+                        ->where('table.contents.type = ?', 'post')
+                        ->where('table.contents.status = ?', 'publish')
+                );
+
+                // 如果该分类下没有文章，或所有文章都有密码，则跳过
+                if (
+                    empty($postsInCategory) || !array_filter($postsInCategory, fn($post) => empty($post['password']))
+                ) {
+                    continue;
+                }
+
+                $sitemap .= <<<EOF
+            <url>
+                <loc>{$categories->permalink}</loc>
+                <changefreq>{$freq}</changefreq>
+                <priority>0.6</priority>
+            </url>
+EOF;
+            }
+        }
+
+        // add tags
+        if (in_array('tags', $this->options->plugin('Sitemap')->sitemapBlock)) {
+            $tags = Cloud::alloc();
+            $freq = $this->options->plugin('Sitemap')->updateFreq;
+
+            while ($tags->next()) {
+                // 检查该标签下是否有非加密文章
+                $postsInTag = $this->db->fetchAll(
+                    $this->db->select('password')
+                        ->from('table.contents')
+                        ->join('table.relationships', 'table.contents.cid = table.relationships.cid')
+                        ->where('table.relationships.mid = ?', $tags->mid)
+                        ->where('table.contents.type = ?', 'post')
+                        ->where('table.contents.status = ?', 'publish')
+                );
+
+                // 如果该标签下没有文章，或所有文章都有密码，则跳过
+                if (
+                    empty($postsInTag) || !array_filter($postsInTag, fn($post) => empty($post['password']))
+                ) {
+                    continue;
+                }
+
+                $sitemap .= <<<EOF
+            <url>
+                <loc>{$tags->permalink}</loc>
+                <changefreq>{$freq}</changefreq>
+                <priority>0.4</priority>
+            </url>
+EOF;
+            }
+        }
+
+        $sitemap .= '</urlset>';
+
+        $this->response->throwContent($sitemap, 'text/xml');
+    }
+}
